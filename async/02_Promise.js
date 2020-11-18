75ff946766bb01414f82a2a016b080281ccb1b49
yiluhukai <31380049+yiluhukai@users.noreply.github.com>
2020-11-18 23:19:28 +0800

    promise test

--- async/02_Promise.js
+++ async/02_Promise.js
@@ -1,7 +1,7 @@
 //Promise的链式调用
 const ajax = require('./01_Promise.js')
 
-ajax('http://localhost:8080/ping')
+ajax('/api/employees')
 	.then(res => {
 		console.log(res)
 	})
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,21 @@
-module.export = {
-	mode: ''
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+	mode: 'none',
+	stats: 'none',
+	devtool: 'source-map',
+	plugins: [new HtmlWebpackPlugin()],
+	devServer: {
+		contentBase: './',
+		index: 'index.html',
+		filename: 'bundle.js',
+		proxy: {
+			'/api': {
+				target: 'http://localhost:8080/',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': '/api'
+				}
+			}
+		}
+	}
 }
