b50cdf47129ed63f3f6a54e3cda70765a6154c11
yiluhukai <31380049+yiluhukai@users.noreply.github.com>
2021-03-28 19:55:13 +0800

    ts-demo

---
+++ vue-ts-demo/src/components/Button.vue
@@ -0,0 +1,27 @@
+<!--  -->
+<template>
+	<div>
+		<button>{{ message }}</button>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+export default Vue.extend({
+	name: "Button",
+	props: {
+		// 类型断言
+		size: String as PropType<"small" | "middle" | "big">
+	},
+	data() {
+		return {
+			message: "foo"
+		};
+	},
+	created() {
+		this.message = "123";
+		this.size === "small";
+	}
+});
+</script>
+<style lang="scss" scoped></style>
